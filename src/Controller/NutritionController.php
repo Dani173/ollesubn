901232537e85f16c9ece940fea20792b5053aab6
linux <dani173gonzalez@gmail.com>
2019-05-30 11:58:34 +0200

    subn

--- src/Controller/NutritionController.php
+++ src/Controller/NutritionController.php
@@ -16,4 +16,64 @@ class NutritionController extends AbstractController
             'controller_name' => 'NutritionController',
         ]);
     }
+
+    /**
+     * @Route("/nutrition/fibra", name="fibra")
+     */
+    public function fibra()
+    {
+        return $this->render('nutrition/fibra.html.twig', [
+            'controller_name' => 'NutritionController',
+        ]);
+    }
+
+    /**
+     * @Route("/nutrition/ayuno", name="ayuno")
+     */
+    public function ayuno()
+    {
+        return $this->render('nutrition/ayuno.html.twig', [
+            'controller_name' => 'NutritionController',
+        ]);
+    }
+
+    /**
+     * @Route("/nutrition/prepost", name="prepost")
+     */
+    public function prepost()
+    {
+        return $this->render('nutrition/prepost.html.twig', [
+            'controller_name' => 'NutritionController',
+        ]);
+    }
+
+    /**
+     * @Route("/nutrition/calorias", name="calorias")
+     */
+    public function calorias()
+    {
+        return $this->render('nutrition/calorias.html.twig', [
+            'controller_name' => 'NutritionController',
+        ]);
+    }
+
+    /**
+     * @Route("/nutrition/agua", name="agua")
+     */
+    public function agua()
+    {
+        return $this->render('nutrition/agua.html.twig', [
+            'controller_name' => 'NutritionController',
+        ]);
+    }
+
+    /**
+     * @Route("/nutrition/alcohol", name="alcohol")
+     */
+    public function alcohol()
+    {
+        return $this->render('nutrition/alcohol.html.twig', [
+            'controller_name' => 'NutritionController',
+        ]);
+    }
 }
